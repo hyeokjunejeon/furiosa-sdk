ada0f2ad94045b9394580c37aafd09770cd2f66b
Hyeokjune Jeon <hyeokjune.jeon@furiosa.ai>
2022-05-31 15:39:54 +0900

    pytest

--- python/furiosa-models/tests/integration/test_vision.py
+++ python/furiosa-models/tests/integration/test_vision.py
@@ -18,25 +18,25 @@ async def test_resnet50_session_wrapper():
     dir_path = os.path.dirname(os.path.realpath(__file__))
     model: Model = synchronous(MLCommonsResNet50)()
     with SessionWrapper(model) as sess:
-        result = sess.inference( cv2.imread( os.path.join(dir_path, 'assets/EgyptianCat.jpg')) )
+        result = sess.inference( os.path.join(dir_path, 'assets/EgyptianCat.jpg') )
     assert result == "Egyptian cat"
 
-#@pytest.mark.asyncio
-#async def test_resnet50():
-#    model: Model = await MLCommonsResNet50()
-#    assert model.name == "MLCommonsResNet50"
+@pytest.mark.asyncio
+async def test_resnet50():
+    model: Model = await MLCommonsResNet50()
+    assert model.name == "MLCommonsResNet50"
 
-#@pytest.mark.asyncio
-#async def test_mobilenet():
-#    model: Model = await MLCommonsSSDMobileNet()
-#    assert model.name == "MLCommonsSSDMobileNet"
+@pytest.mark.asyncio
+async def test_mobilenet():
+    model: Model = await MLCommonsSSDMobileNet()
+    assert model.name == "MLCommonsSSDMobileNet"
 
-#@pytest.mark.asyncio
-#async def test_resnet34():
-#    model: Model = await MLCommonsSSDResNet34()
-#    assert model.name == "MLCommonsSSDResNet34"
+@pytest.mark.asyncio
+async def test_resnet34():
+    model: Model = await MLCommonsSSDResNet34()
+    assert model.name == "MLCommonsSSDResNet34"
 
-#@pytest.mark.asyncio
-#async def test_efficientnet():
-#    model: Model = await EfficientNetV2_S()
-#    assert model.name == "EfficientNetV2_S"
+@pytest.mark.asyncio
+async def test_efficientnet():
+    model: Model = await EfficientNetV2_S()
+    assert model.name == "EfficientNetV2_S"
